bbda054ca33fa2717f1733bead79e71aa558e5d1
geony <purscean@gmail.com>
2023-02-04 12:06:14 +0900

    gitignore except secret

---
+++ back/secret.js
@@ -0,0 +1,9 @@
+exports.jwtSecret = "jwt-secret-8520";
+
+exports.databaseSecret={
+    host: "www.donggeon.store",
+    user: "dummy",
+    port: "3306",
+    password: "111111",
+    database: "MyTodoDB"
+};
